be5c59dbd644211d31464fa2bbdc955640cb14f2
Gaëlle <gaellepoisson.95@gmail.com>
2026-01-11 22:45:23 +0100

    Add files via upload

---
+++ ressources/includes/footer.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="footer-container">
+    <div class="footer-content">
+      <div class="logo">
+        <img src="./ressources/images/logo-cyu-couleurs.png" alt="Logo CY Université" />
+      </div>
+      <div class="footer-text">
+        <p>Certains textes sont issus de Wikipédia</p>
+        <a href="https://fr-fr.facebook.com/CYCergyParisUniversite/">Facebook</a> |
+        <a href="https://twitter.com/universitecergy?lang=fr">Twitter</a> |
+        <a href="https://www.cyu.fr/">Université CY Paris Université</a>
+        <p>© 2013-2021 - BUT MMI - CY Paris Université - SAE 105</p>
+      </div>
+    </div>
+    <div class="footer-link">
+      <a href="#top">Remonter</a>
+    </div>
+  </div>
+</footer>
---
+++ ressources/includes/header.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="header.css">
+<nav class="navigation-principale">
+    <ul>
+        <li>
+            <a href="index.php">ACCUEIL</a>
+        </li>
+        <li >
+            <a href="apropos.php">A PROPOS</a>
+        </li>
+        <li>
+            <a href="lieuxdevie.php"> LIEUX DE VIE</a>
+        </li>
+        <li>
+            <a href="surlesmedias.php">SUR LES MÉDIAS</a>
+        </li>
+        <li>
+            <a href="contact.php">CONTACT</a>
+        </li>
+        <li>
+            <a href="debouches.php">DÉBOUCHÉS</a>
+        </li>
+    </ul>
+</nav>
+
+<header class="bulle">
+    <article class="titre">
+        <p class="txt-grand">
+            <span>Bachelor</span>
+            <span>Universitaire de</span>
+            <span>Technologie</span>
+        </p>
+        <p class="txt-petit">
+            <span>Métiers du Multimédia et de</span>
+            <span>l'Internet</span>
+        </p>
+    </article>
+
+    <article class="bulle-icone <?php echo $couleur_bulle_classe; ?>"></article>
+    <article class="bulle-icone-bordure <?php echo $couleur_bulle_classe; ?>"></article>
+</header>
